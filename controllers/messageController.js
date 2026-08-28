677573b955ace8a2460d22feafd29fb57725f606
pociello <asierpociello@gmail.com>
2026-08-28 10:53:32 +0200

    feat: create message details view & routing

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,31 +1,49 @@
 // initial data
+let next_message_Id = 2;
 const messages = [
   {
     user: 'Nick',
     text: 'hey, the server is up and running',
     added: new Date(),
+    message_id: 0,
   },
   {
     user: 'Andrew',
     text: 'great, we can start talking now',
     added: new Date(),
+    message_id: 1,
   },
 ];
 
-const message_index = (req, res) =>
+const message_index = (req, res) => {
   res.render('index', { messages: messages, title: 'messages' });
+};
 
-const message_new_get = (req, res) =>
+const message_details = (req, res) => {
+  const id = Number(req.params.id);
+  const message = messages.find((m) => m.message_id === id);
+
+  res.render('details', { title: 'details', message: message });
+};
+
+const message_new_get = (req, res) => {
   res.render('form', { title: 'new message' });
+};
 
 const message_new_post = (req, res) => {
   const { messageUser, messageText } = req.body;
-  messages.push({ user: messageUser, text: messageText, added: new Date() });
+  messages.push({
+    user: messageUser,
+    text: messageText,
+    added: new Date(),
+    message_id: next_message_Id++,
+  });
   res.redirect('/messages');
 };
 
 module.exports = {
   message_index,
+  message_details,
   message_new_get,
   message_new_post,
 };
--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {
   message_index,
+  message_details,
   message_new_get,
   message_new_post,
 } = require('../controllers/messageController.js');
@@ -12,5 +13,7 @@ router.get('/', message_index);
 router.get('/new', message_new_get);
 
 router.post('/new', message_new_post);
+
+router.get('/:id', message_details);
 
 module.exports = router;
